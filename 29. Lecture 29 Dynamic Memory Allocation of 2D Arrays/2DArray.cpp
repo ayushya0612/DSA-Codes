6211f5d24276e0baa623817ee382fff67da0629b
Ayush Singh <ayushya0612@gmail.com>
2023-02-07 00:25:33 +0530

    Dynamic Memory Allocation of 2D array

---
+++ 29. Lecture 29 Dynamic Memory Allocation of 2D Arrays/2DArray.cpp
@@ -0,0 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    /*   -- if array is nXn (row=col)
+    int n;
+    cin >> n;
+
+    // Creating a 2D array
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    // Taking input
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+        
+    }
+
+    cout << endl;
+
+    // Taking output
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] <<" ";
+        }
+        cout << endl;
+        
+    }
+    */
+
+   /*  if array has row and col different (n!=m)
+   int row, col;
+   cin >> row >> col;
+
+   // Creating the 2D array
+   int** arr = new int* [row];
+   for (int i = 0; i < row; i++)
+   {
+        arr[i] = new int[col];
+   }
+
+   //taking input
+   for (int i = 0; i < row; i++)
+   {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+        
+   }
+
+   cout << endl;
+
+   //taking output
+   for (int i = 0; i < row; i++)
+   {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+        
+   }
+
+   // releasing memory
+   for (int i = 0; i < row; i++)
+   {
+        delete [] arr[i];
+   }
+
+   delete []arr;
+   
+   */
+   
+   
+    
+    
+    
+} 
